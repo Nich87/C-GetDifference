970affca34b21a7840ad4e75780e85228fc83916
csjsc <furandolltyan@gmail.com>
2021-10-15 23:05:17 +0900

    コメントの追加

--- pre3.c
+++ pre3.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define THROW_ERROR(message) do { fprintf(stderr, "Error: %s:%d: %s\n", __FILE__, __LINE__, message); exit(1); } while(0)
+#define THROW_ERROR(message) do { fprintf(stderr, "%s:%d: error: %s\n", __FILE__, __LINE__, message); exit(1); } while(0)
 
 
 int main(void){
@@ -11,6 +11,7 @@ int main(void){
 	const int month[] = { 1, 12 };
 	const int day[] = { 31, 31 };
 
+	// tm構造体を用意
 	struct tm start = {}, end = {};
 	start.tm_year = year[0] - 1900;
 	start.tm_mon = month[0] - 1;
@@ -19,15 +20,17 @@ int main(void){
 	end.tm_mon = month[1] - 1;
 	end.tm_mday = day[1];
 
+	// それぞれをtime_t型(1970/01/01からの秒数)に変換
 	long start_sec = mktime(&start);
 	long end_sec = mktime(&end);
 
+	// 変換に失敗したらエラーメッセージを出して終了。
 	if (!~start_sec) 
 		THROW_ERROR("Failed to convert 'struct tm' to 'time_t'");
-
 	if (!~end_sec) 
 		THROW_ERROR("Failed to convert 'struct tm' to 'time_t'");
 
+	// 差を計算して、秒を日に変換。
 	printf("Difference: %ld\n", (end_sec - start_sec) / (60 * 60 * 24));
 
 	return 0;
