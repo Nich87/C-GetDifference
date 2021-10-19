62a8f8869044a162b37b36c9ae9a2214590c5e32
Reika <56439343+Nich87@users.noreply.github.com>
2021-10-19 18:21:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,56 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int check_year(int); //関数のプロトタイプ宣言
+int check_year(int); //閏年判定関数のプロトタイプ宣言
+void ymd_input(void); //入力関数のプロトタイプ宣言
 const int month_last[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //月末日
+int year[] = { 0, 0 };
+int month[] = { 0, 0 };
+int day[] = { 0, 0 };
+/*
+* 定数化する場合はここ
+const int year[] = { 2003, 2021 };
+const int month[] = { 12, 10 };
+const int day[] = { 14, 15 };
+*/
 
 int main(void) {
 
-	/*
-	* 定数化する場合はここ
-	const int year[] = { 2003, 2021 };
-	const int month[] = { 12, 10 };
-	const int day[] = { 14, 15 };
-	*/
-
-	int year[] = { 0, 0 };
-	int month[] = { 0, 0 };
-	int day[] = { 0, 0 };
-	
-
-//inputs:
-	while (1){
-		char YN[] = { "" };
-		//do-whileで個別範囲指定すると安全(1-12月まで等)
-		printf("誕生年(西暦)を入力:");
-		scanf("%d", &year[0]);
-		printf("\n誕生月を入力:");
-		scanf("%d", &month[0]);
-		printf("\n誕生日を入力:");
-		scanf("%d", &day[0]);
-		printf("\n指定する年を入力:");
-		scanf("%d", &year[1]);
-		printf("\n指定する月を入力:");
-		scanf("%d", &month[1]);
-		printf("\n指定する日を入力:");
-		scanf("%d", &day[1]);
-		printf("\n\x1b[4m%d年%d月%d日\x1b[0mから\x1b[4m%d年%d月%d日\x1b[0mまでの計算をしますか？(Y/N):", year[0], month[0], day[0], year[1], month[1], day[1]);
-
-		while ('Y' != YN[0] && 'N' != YN[0]){
-			scanf("%c", &YN[0]);
-			if (YN[0] == 'Y') break;
-			else if (YN[0] == 'N') continue;
-		}
-		if (YN[0] == 'Y') break;
-	}
-	/*
-	do {
-		scanf("%c", &YN[0]);
-		if (YN[0] == 'Y') break;
-		else if (YN[0] == 'N') goto inputs;
-	} while ('Y' != YN[0] && 'N' != YN[0]);
-	*/
-
+	ymd_input();
 
 	// 閏年を考慮せずにyear[0]年1月1日からyear[1]年1月1日までの日数を代入
 	int getDay = (year[1] - year[0]) * 365;
@@ -78,7 +44,7 @@ int main(void) {
 	// month[1]月の、1日からday[1]日までの日数を加算
 	getDay += day[1] - 1;
 
-	printf("\n\x1b[4m%d年%d月%d日\x1b[0mから\x1b[4m%d年%d月%d日\x1b[0mまでは\x1b[4m%d日\x1b[0mです。\n", year[0], month[0], day[0], year[1], month[1], day[1], getDay);
+	printf("\n%d年%d月%d日から%d年%d月%d日までは%d日です。\n", year[0], month[0], day[0], year[1], month[1], day[1], getDay);
 
 	return 0;
 }
@@ -93,4 +59,57 @@ int main(void) {
 int check_year(int year) {
 	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) return 1;
 	return 0;
+}
+
+void ymd_input(){
+
+	//inputs:
+	while (1){
+		char YN[] = { "" };
+		//do-whileで個別範囲指定すると安全(1-12月まで等)
+		do{
+			printf("誕生年(西暦)を入力:");
+			scanf("%d", &year[0]);
+		} while (year[0]<0);
+
+		do{
+			printf("\n誕生月を入力:");
+			scanf("%d", &month[0]);
+		} while (month[0] <= 1 && month[0] >= 12);
+		do{
+			printf("\n誕生日を入力:");
+			scanf("%d", &day[0]);
+		} while (day[0] <= 1 && day[0] >= 31);
+
+
+		do{
+			printf("\n指定する年を入力:");
+			scanf("%d", &year[1]);
+		} while (year[1]<0);
+
+		do{
+			printf("\n指定する月を入力:");
+			scanf("%d", &month[1]);
+		} while (month[1] <= 1 && month[1] >= 12);
+		do{
+			printf("\n指定する日を入力:");
+			scanf("%d", &day[1]);
+		} while (day[1] <= 1 && day[1] >= 31);
+
+		printf("\n%d年%d月%d日から%d年%d月%d日までの計算をしますか？(Y/N):", year[0], month[0], day[0], year[1], month[1], day[1]);
+
+		while ('Y' != YN[0] && 'N' != YN[0]){
+			scanf("%c", &YN[0]);
+			if (YN[0] == 'Y') break;
+			else if (YN[0] == 'N') continue;
+		}
+		if (YN[0] == 'Y') break;
+	}
+	/*
+	do {
+	scanf("%c", &YN[0]);
+	if (YN[0] == 'Y') break;
+	else if (YN[0] == 'N') goto inputs;
+	} while ('Y' != YN[0] && 'N' != YN[0]);
+	*/
 }
